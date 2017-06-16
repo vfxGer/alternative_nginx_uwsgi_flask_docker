fa802d9a640a35380a7a2997f2ad9903b1fe6849
Gerard Keating <gerard_keating@symantec.com>
2017-06-16 15:04:25 +0100

    Removes some dependcies that should not have been there

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-
-
 import flask
-from werkzeug.utils import secure_filename
-from PIL import Image
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-UPLOAD_FOLDER = '/client_uploads'
 
 app = flask.Flask(__name__)
+
+
 @app.route('/')
 def hello():
     return "hello world" 
@@ -18,3 +12,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 80, debug=True)
+
